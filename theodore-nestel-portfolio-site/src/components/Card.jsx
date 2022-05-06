c91d3f4b654be8efc7dc27df6ed45fc47161af8c
Arjun Patel <47369740+arjunsamir@users.noreply.github.com>
2022-05-06 11:48:16 -0400

    added card

---
+++ theodore-nestel-portfolio-site/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import bemify from "../../utils/bemify";
+
+const bem = bemify("card");
+
+const Card = (props) => {
+
+  return (
+    <div className={bem()}>
+
+    </div>
+  )
+
+}
+
+
+export default Card;
