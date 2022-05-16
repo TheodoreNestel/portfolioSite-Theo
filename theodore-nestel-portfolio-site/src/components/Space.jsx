2dac38eb1c81f22ee3030f0820c6dfde1bce96ab
Arjun Patel <47369740+arjunsamir@users.noreply.github.com>
2022-05-16 11:45:42 -0400

    Set up components

---
+++ theodore-nestel-portfolio-site/src/components/Space.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Space = () => {
+
+  return (
+    <div>Imagine a black hole here</div>
+  )
+
+}
+
+
+export default Space;
--- theodore-nestel-portfolio-site/src/pages/index.js
+++ theodore-nestel-portfolio-site/src/pages/index.js
@@ -1,15 +1,20 @@
 import * as React from "react"
-import "../sass/main.scss"
-
 
 // styles
 //we removed the inline styles in favor of stylesheet for the sake of code clarity 
+
+// Import Main SCSS Entry Point Stylesheet
+import "../sass/main.scss"
+
+// Import Page Components
+import Space from "../components/Space"
 
 
 // markup this is where the "react app" is edited like we know how it works
 const IndexPage = () => {
   return (
     <main>
+      <Space />
       <div>
         <h1>Hello world Gatsby style!</h1>
         <img src="https://c.tenor.com/L9Geb6hCmCEAAAAM/dance-move.gif"/>
