39a9660d1cdd7a89c4f93d1d5b169b2678e20977
TheodoreNestel <73968042+TheodoreNestel@users.noreply.github.com>
2022-06-06 11:45:03 -0400

    Revert "Updated animation with layoutEffects"
    This reverts commit 58d4d716d37e45881784c17be9c5b48f00593bfb.

--- theodore-nestel-portfolio-site/src/utility/useClickOut.js
+++ theodore-nestel-portfolio-site/src/utility/useClickOut.js
@@ -1,22 +1,17 @@
 import {useEffect} from "react" 
 
-export default function useClickOut(selector , callback, deps) {
+export default function useClickOut(selector , callback){
+
 
     useEffect(()=>{
-
         const handleClick = (e)=>{
-
-            // if the clicked element is not the selector, and the clicked element is not a child of the selector
-            const isMatchedElement = e.target.closest(selector)
+           const isMatchedElement = e.target.closest(selector)
             
-            !isMatchedElement && callback && callback() //checks that callback is a thing and not null and that isTarget isnt true
+           !isMatchedElement && callback && callback() //checks that callback is a thing and not null and that isTarget isnt true
           
+
         }
-
         document.addEventListener("click" , handleClick)
-
         return ()=> document.removeEventListener("click", handleClick)
-
-    }, deps)
-    
+    },[selector])
 }
